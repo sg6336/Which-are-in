34b2b52461c09d9c10609fa48cc090f7ff95941e
Denys <navrotskyidenys@gmail.com>
2022-09-12 21:09:12 +0300

    Add first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a1 = new string[] { "arp", "live", "strong" };
+            string[] a2 = new string[] { "lively", "alive", "harp", "sharp", "armstrong" };
+            string[] r = new string[] { "arp", "live", "strong" };
+            WhichAreIn.inArray(a1, a2);
+        }
+    }
+
+    public class WhichAreIn
+    {
+        public static string[] inArray(string[] array1, string[] array2)
+        {
+            List<string> result = new List<string>();
+            string str = String.Join(" ", array2);
+            foreach (string item in array1)
+            {
+                if (str.Contains(item) == true)
+                {
+                    result.Add(item);
+                }
+            }
+            result.Sort();
+            return result.ToArray();
+        }
+    }
+}
---
+++ Which are in/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit.Framework;
+using ConsoleApp1;
+
+[TestFixture]
+public class WhichAreInTests
+{
+
+    [Test]
+    public void Test1()
+    {
+        string[] a1 = new string[] { "arp", "live", "strong" };
+        string[] a2 = new string[] { "lively", "alive", "harp", "sharp", "armstrong" };
+        string[] r = new string[] { "arp", "live", "strong" };
+        Assert.AreEqual(r, WhichAreIn.inArray(a1, a2));
+    }
+}
